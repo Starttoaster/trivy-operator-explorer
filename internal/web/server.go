e027cad4a9aca50618c302299df17f8248491eaa
Starttoaster <bmbawb@gmail.com>
2024-04-29 20:43:48 -0700

    Add exposed secrets dashboard (#22)

--- internal/web/server.go
+++ internal/web/server.go
@@ -16,6 +16,8 @@ import (
 	clusterrolesview "github.com/starttoaster/trivy-operator-explorer/internal/web/views/clusterroles"
 	configauditview "github.com/starttoaster/trivy-operator-explorer/internal/web/views/configaudit"
 	configauditsview "github.com/starttoaster/trivy-operator-explorer/internal/web/views/configaudits"
+	exposedsecretview "github.com/starttoaster/trivy-operator-explorer/internal/web/views/exposedsecret"
+	exposedsecretsview "github.com/starttoaster/trivy-operator-explorer/internal/web/views/exposedsecrets"
 	imageview "github.com/starttoaster/trivy-operator-explorer/internal/web/views/image"
 	imagesview "github.com/starttoaster/trivy-operator-explorer/internal/web/views/images"
 	roleview "github.com/starttoaster/trivy-operator-explorer/internal/web/views/role"
@@ -27,14 +29,16 @@ func Start(port string) error {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", imagesHandler)
 	mux.HandleFunc("/image", imageHandler)
-	mux.HandleFunc("/roles", rolesHandler)
-	mux.HandleFunc("/role", roleHandler)
-	mux.HandleFunc("/clusterroles", clusterrolesHandler)
-	mux.HandleFunc("/clusterrole", clusterroleHandler)
 	mux.HandleFunc("/configaudits", configauditsHandler)
 	mux.HandleFunc("/configaudit", configauditHandler)
 	mux.HandleFunc("/clusteraudits", clusterauditsHandler)
 	mux.HandleFunc("/clusteraudit", clusterauditHandler)
+	mux.HandleFunc("/clusterroles", clusterrolesHandler)
+	mux.HandleFunc("/clusterrole", clusterroleHandler)
+	mux.HandleFunc("/exposedsecrets", exposedsecretsHandler)
+	mux.HandleFunc("/exposedsecret", exposedsecretHandler)
+	mux.HandleFunc("/roles", rolesHandler)
+	mux.HandleFunc("/role", roleHandler)
 	mux.Handle("/static/", http.FileServer(http.FS(content.Static)))
 	return http.ListenAndServe(fmt.Sprintf(":%s", port), mux)
 }
@@ -437,6 +441,85 @@ func clusterauditHandler(w http.ResponseWriter, r *http.Request) {
 	err = tmpl.Execute(w, audit)
 	if err != nil {
 		log.Logger.Error("encountered error executing clusteraudit html template", "error", err)
+		http.Error(w, "Internal Server Error, check server logs", http.StatusInternalServerError)
+		return
+	}
+}
+
+func exposedsecretsHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFS(content.Static, "static/exposedsecrets.html", "static/sidebar.html"))
+	if tmpl == nil {
+		log.Logger.Error("encountered error parsing exposed secrets html template")
+		http.Error(w, "Internal Server Error, check server logs", http.StatusInternalServerError)
+		return
+	}
+
+	data, err := kube.GetExposedSecretReportList()
+	if err != nil {
+		log.Logger.Error("error getting ExposedSecretReports", "error", err.Error())
+		return
+	}
+	imageData := exposedsecretsview.GetView(data)
+
+	err = tmpl.Execute(w, imageData)
+	if err != nil {
+		log.Logger.Error("encountered error executing exposed secrets html template", "error", err)
+		http.Error(w, "Internal Server Error, check server logs", http.StatusInternalServerError)
+		return
+	}
+}
+
+func exposedsecretHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFS(content.Static, "static/exposedsecret.html", "static/sidebar.html"))
+	if tmpl == nil {
+		log.Logger.Error("encountered error parsing exposed secret html template")
+		http.Error(w, "Internal Server Error, check server logs", http.StatusInternalServerError)
+		return
+	}
+
+	// Parse URL query params
+	q := r.URL.Query()
+
+	// Check query params -- 404 if required params not passed
+	imageName := q.Get("image")
+	if imageName == "" {
+		log.Logger.Error("image name query param missing from request")
+		http.NotFound(w, r)
+		return
+	}
+	imageDigest := q.Get("digest")
+	if imageDigest == "" {
+		log.Logger.Error("image digest query param missing from request")
+		http.NotFound(w, r)
+		return
+	}
+	severity := q.Get("severity")
+
+	// Get secret reports
+	data, err := kube.GetExposedSecretReportList()
+	if err != nil {
+		log.Logger.Error("error getting ExposedSecretReports", "error", err.Error())
+		return
+	}
+
+	// Get image view from reports
+	view, found := exposedsecretview.GetView(data, exposedsecretview.Filters{
+		Name:     imageName,
+		Digest:   imageDigest,
+		Severity: severity,
+	})
+
+	// If the selected image from query params was not found, 404
+	if !found {
+		log.Logger.Error("image name and digest query params did not produce a valid result from scraped data", "image", imageName, "digest", imageDigest)
+		http.NotFound(w, r)
+		return
+	}
+
+	// Execute html template
+	err = tmpl.Execute(w, view)
+	if err != nil {
+		log.Logger.Error("encountered error executing exposed secret html template", "error", err)
 		http.Error(w, "Internal Server Error, check server logs", http.StatusInternalServerError)
 		return
 	}
