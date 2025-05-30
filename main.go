c5c93765ab5ab76d659cf3daf28e102b04bfaf46
Starttoaster <b.butler@chia.net>
2025-05-29 22:08:39 -0700

    Add hasfix toggle

--- main.go
+++ main.go
@@ -26,6 +26,7 @@ import (
 //go:embed static/css/output.css
 //go:embed static/css/extra.css
 //go:embed static/js/chart.js
+//go:embed static/js/images-hasfix.js
 var static embed.FS
 
 func main() {
