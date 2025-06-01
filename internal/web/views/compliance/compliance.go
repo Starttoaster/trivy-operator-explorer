4ac53d38a627259d3154c4bf13b0d83cda4aa292
Starttoaster <b.butler@chia.net>
2025-06-01 14:45:44 -0700

    Properly sort compliance report table

--- internal/web/views/compliance/compliance.go
+++ internal/web/views/compliance/compliance.go
@@ -2,8 +2,10 @@ package compliance
 
 import (
 	"fmt"
-	"github.com/aquasecurity/trivy-operator/pkg/apis/aquasecurity/v1alpha1"
+	"sort"
 	"strings"
+
+	"github.com/aquasecurity/trivy-operator/pkg/apis/aquasecurity/v1alpha1"
 )
 
 // GetView converts some report data to the /compliancereports view
@@ -107,17 +109,17 @@ func GetSingleReportData(data *v1alpha1.ClusterComplianceReportList, id string, 
 
 		// Loop through the spec of the report adding items to the map using the id number of the check as the key
 		for _, specCheck := range item.Spec.Compliance.Controls {
+			// If the severity filter was given, make sure we have the right severity for this check
+			if severity != nil && !strings.EqualFold(*severity, string(specCheck.Severity)) {
+				continue
+			}
+
 			var ids []CheckID
 			for _, checkID := range specCheck.Checks {
 				ids = append(ids, CheckID{
 					ID:  strings.ToUpper(checkID.ID),
 					URL: fmt.Sprintf("https://avd.aquasec.com/misconfig/kubernetes/general/%s/", strings.ToLower(checkID.ID)),
 				})
-			}
-
-			// If the severity filter was given, make sure we have the right severity for this check
-			if severity != nil && *severity != string(specCheck.Severity) {
-				continue
 			}
 
 			checksMap[specCheck.ID] = Check{
@@ -168,6 +170,26 @@ func GetSingleReportData(data *v1alpha1.ClusterComplianceReportList, id string, 
 	}
 
 	// Sort the r.Checks by Severity and then TotalFailed
+	sort.Slice(r.Checks, func(i, j int) bool {
+		// First compare by severity
+		if r.Checks[i].Severity != r.Checks[j].Severity {
+			// Define severity order: CRITICAL > HIGH > MEDIUM > LOW > UNKNOWN
+			severityOrder := map[string]int{
+				"CRITICAL": 0,
+				"HIGH":     1,
+				"MEDIUM":   2,
+				"LOW":      3,
+				"UNKNOWN":  4,
+			}
+			return severityOrder[r.Checks[i].Severity] < severityOrder[r.Checks[j].Severity]
+		}
+		// If severity is the same, compare by TotalFailed
+		if r.Checks[i].TotalFailed != nil && r.Checks[j].TotalFailed != nil {
+			return *r.Checks[i].TotalFailed > *r.Checks[j].TotalFailed
+		}
+		// If one is nil, prioritize the non-nil value
+		return r.Checks[i].TotalFailed != nil
+	})
 
 	return r
 }
