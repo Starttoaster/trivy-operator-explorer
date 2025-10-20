250e8aeb34eaa80d324fc90e8de8e690ad5d0c87
Starttoaster <b.butler@chia.net>
2025-10-19 21:04:26 -0700

    Remove hidden CVE slider from sidebar on images path

--- static/js/images-hasfix.js
+++ static/js/images-hasfix.js
@@ -8,29 +8,12 @@ function handleHasFixChange(checkbox) {
     window.location.href = url.toString();
 }
 
-function handleShowIgnoredChange(checkbox) {
-    const url = new URL(window.location.href);
-    if (checkbox.checked) {
-        url.searchParams.set('showignored', 'true');
-    } else {
-        url.searchParams.delete('showignored');
-    }
-    window.location.href = url.toString();
-}
-
-// Check URL parameters on page load
+// Check hasfix URL parameter on page load
 window.onload = function() {
     const url = new URL(window.location.href);
-    
-    // Check hasfix parameter
+
     const hasFixCheckbox = document.getElementById('hasFixCheckbox');
     if (hasFixCheckbox) {
         hasFixCheckbox.checked = url.searchParams.get('hasfix') === 'true';
-    }
-    
-    // Check showignored parameter
-    const showIgnoredCheckbox = document.getElementById('showIgnoredCheckbox');
-    if (showIgnoredCheckbox) {
-        showIgnoredCheckbox.checked = url.searchParams.get('showignored') === 'true';
     }
 }
